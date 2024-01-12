0222743bde2ee5cee9df9908b050f2b2aa897748
Xaiodo <rostislavgovbak@gmail.com>
2024-01-12 16:40:13 +0200

    Created basic app structure.

---
+++ lib/app/app_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[],
+          ),
+        ),
+      );
+}
---
+++ lib/src/pages/lift_page/lift_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LiftView extends StatelessWidget {
+  const LiftView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
