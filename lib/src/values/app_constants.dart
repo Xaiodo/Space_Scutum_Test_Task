b6224c3c1f0c2b6be98ff18a14573ed9dd2281f1
Xaiodo <rostislavgovbak@gmail.com>
2024-01-15 14:42:07 +0200

    Updated app constants and theme.

--- lib/src/values/app_constants.dart
+++ lib/src/values/app_constants.dart
@@ -1,7 +1,13 @@
 class AppConstants {
-  static const String desingedBy = 'Rostyslav';
+  static const String desingedBy = 'designed by Rostyslav';
 
   static const String databaseName = 'SpaceTask.db';
 
+  static const String applicationEnterPageTitle = 'Test Task';
+
+  static const String enterButtonText = 'Enter';
+
   static const String housesTableName = 'Houses';
+
+  static const String catImagePath = 'assets/images/cat.png';
 }
--- lib/src/values/theme.dart
+++ lib/src/values/theme.dart
@@ -5,8 +5,7 @@ final theme = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: AppColors.white,
     primary: AppColors.primary,
-    background: AppColors.primary,
-    surface: Colors.red,
+    background: AppColors.backgroundColor,
   ),
   dialogTheme: dialogTheme,
   elevatedButtonTheme: elevatedButtonTheme,
@@ -37,5 +36,23 @@ final dialogTheme = DialogTheme(
   backgroundColor: AppColors.modalBackgroundColor,
   shape: RoundedRectangleBorder(
     borderRadius: BorderRadius.circular(0),
+  ),
+);
+
+const textTheme = TextTheme(
+  headlineMedium: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  ),
+  bodyLarge: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.black,
   ),
 );
