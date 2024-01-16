dca03b4c9c9336641aedcaf6e88dc992db966c43
Xaiodo <rostislavgovbak@gmail.com>
2024-01-16 19:04:20 +0200

    Created house repository.

---
+++ lib/src/repositories/houses_repository.dart
@@ -0,0 +1,28 @@
+import 'package:space_scutum_test_task/src/models/house.dart';
+import 'package:space_scutum_test_task/src/values/app_constants.dart';
+import 'package:sqflite/sqflite.dart';
+
+class HouseRepository {
+  final Database _database;
+
+  const HouseRepository({required Database database}) : _database = database;
+
+  Future<List<House>> getHouses() async {
+    final List<Map<String, Object?>> result = await _database.query(
+      AppConstants.housesTableName,
+    );
+    final houses = result.map((e) => House.fromJson(e)).toList();
+    return houses;
+  }
+
+  Future<List<House>> addHouse(String name, int floorCount) async {
+    await _database.insert(AppConstants.housesTableName, {
+      'name': name,
+      'floorCount': floorCount,
+      'selectedFloorNumber': 0,
+    });
+
+    final houses = await getHouses();
+    return houses;
+  }
+}
